bd89db8f091a8ae3646efc4952c246e8e95ef628
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-05-09 19:00:31 +0200

    chore: remove obsolete folders after move to src/

--- config_factory/logger_config.py
+++
@@ -1,9 +0,0 @@
-
-from dataclasses import dataclass
-from .base import Config
-@Config.register("logging_config")
-@dataclass
-class LoggingConfig(Config):
-    mlflow_uri       : str = "file:./mlruns"
-    experiment_name  : str = "default_exp"
-    run_name         : str = "run"
--- utils/load_config.py
+++
@@ -1,6 +0,0 @@
-from config_factory.runner_config import RunnerConfig
-
-def load_and_finalize(path: str) -> RunnerConfig:
-    cfg = RunnerConfig.from_yaml(path)
-    cfg.finalize()
-    return cfg
