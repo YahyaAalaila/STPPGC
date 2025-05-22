b4eece1aeec7e39e5a20b97b2830ca89ada4a3a1
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-05-22 12:53:50 +0200

    modify setup.py temp

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+# setup.py
 import numpy
-from setuptools import setup, Extension
+from setuptools import setup, find_packages, Extension
 
 ext_modules = [
-    Extension(
-        "lib.neural_stpp.data_utils_fast",
-        ["src/lib/neural_stpp/data_utils_fast.pyx"],
-        include_dirs=[numpy.get_include()],
-        extra_compile_args=["-O3"],
-    )
+  Extension(
+    "lib.neural_stpp.data_utils_fast",
+    ["src/lib/neural_stpp/data_utils_fast.pyx"],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args=["-O3"],
+  )
 ]
 
-setup( 
-    # … copy the same metadata from your pyproject …
-    ext_modules=ext_modules,
+setup(
+  name="lightning-stpp",
+  version="0.0.1",
+  package_dir={"": "src"},
+  packages=find_packages(where="src"),
+  ext_modules=ext_modules,
+  install_requires=["torch>=2.2", "lightning>=2.2", "ray[tune]>=2.9"],
 )
