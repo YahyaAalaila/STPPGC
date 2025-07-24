eee67251be3dcee49217d4751e763211e0ab3606
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-07-24 10:06:02 +0200

    New model: Diffusion STPP

--- src/lightning_stpp/data/__init__.py
+++ src/lightning_stpp/data/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .neuralstpp_dm import NeuralSTPPDataModule
 from .deepstpp_dm import DeepSTPPDataModule
 from .smash_dm import SMASHDataModule
+from .diffstpp_dm import DiffSTPPDataModule
 from .base import LightDataModule
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "DeepSTPPDataModule",
     "LightDataModule",
     "SMASHDataModule",
+    "DiffSTPPDataModule"
 ]
